71744495f481c40face0261595268a1ef0b7bbd9
mohamedwassim.bezine <mohamedwassim.bezine@neoxam.com>
2021-11-14 14:10:01 +0100

    charts patient

--- core/settings.py
+++ core/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 #}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'patient_tr',
-        'USER': 'postgres',
-        'PASSWORD': 'adminadmin',
-        'HOST': '172.22.0.2',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 # Password validation
