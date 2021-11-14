57756c11bbe8bf05652b50c836eebadeaac88a08
mohamedwassim.bezine <mohamedwassim.bezine@neoxam.com>
2021-11-14 17:03:27 +0100

    charts patient

--- core/settings.py
+++ core/settings.py
@@ -70,22 +70,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'patient_tr',
-#        'USER': 'postgres',
-#        'PASSWORD': 'adminadmin',
-#        'HOST': '172.22.0.2',
-#        'PORT': '5432',
-#    }
-#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'patient_tr',
+        'USER': 'postgres',
+        'PASSWORD': 'adminadmin',
+        'HOST': '172.22.0.2',
+        'PORT': '5432',
     }
 }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
