721328b3721508a2a2ed55ec3deb2b4fe120b74b
mohamedwassim.bezine <mohamedwassim.bezine@neoxam.com>
2022-01-24 15:43:16 +0100

    add 404 exceptions to all the function and configure the 404 page redirect

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -4,5 +4,32 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.db import models
+GENDER_CHOICES =(
+    ("Masculin", "Masculin"),
+    ("Féminin", "Féminin"),
+)
+Roles=(
+    ("Administrateur", "Administrateur"),
+    ("Secrétaire", "Secrétaire"),
+)
 
+STATUT_MATRIMONIAL_CHOICES=(
+    ('Celibataire','Celibataire'),
+    ('Mariée','Mariée'),
+    ('Veuve','Veuve'),
+    ('Divorcée','Divorcée'),
+)
 # Create your models here.
+class Employee(models.Model):
+    Nom = models.CharField(max_length=200)
+    Prenom = models.CharField(max_length=200)
+    Username = models.CharField(max_length=200)
+    Email = models.CharField(max_length=200)
+    Date_de_naissance = models.DateField()
+    Lieu_de_naissance = models.CharField(max_length=200)
+    Role = models.CharField(max_length=200,choices=Roles)
+    Adresse = models.CharField(max_length=200)
+    Cin = models.IntegerField(primary_key= True)
+    Sexe = models.CharField(max_length=200,choices=GENDER_CHOICES)
+    Statut_matrimonial = models.CharField(max_length=200,choices=STATUT_MATRIMONIAL_CHOICES)
+    Telephone = models.IntegerField()
